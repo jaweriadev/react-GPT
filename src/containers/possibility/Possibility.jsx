fae2a2b70d2335c28855f9dc0e407bb1ba18f3e7
jaweriadev <jaweriaabdulkhaliq@gmail.com>
2023-07-11 13:46:18 +0500

    updated

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import PossibilityImage from "../../assets/possibility.png";
 import "./possibility.css";
 const Possibility = () => {
-  return <div></div>;
+  return (
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src={PossibilityImage} alt="possibility" />
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
